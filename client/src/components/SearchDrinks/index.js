752c43a17979a6914cbd7cc22a42025dc4ec84f8
Shatikka McKnight <32470118+shatikka-mcknight@users.noreply.github.com>
2021-07-21 00:23:08 -0400

    update nonsuer page to populate on one  page

--- client/src/components/SearchDrinks/index.js
+++ client/src/components/SearchDrinks/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 // import { useHistory } from "react-router";
 
@@ -6,7 +6,8 @@ const SearchDrinks = () => {
   const [city, updateCity] = useState("");
   const [drink, updateDrink] = useState({});
   const [weather, updateWeather] = useState({});
-  //const drinkData = 
+  //const numberDrink=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+  //const drinkData =
 
   function getTheWeather() {
     // uses the city "state" variable to fill out the queryURL's
@@ -26,6 +27,44 @@ const SearchDrinks = () => {
     const drinkAPI =
       "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
     getDrink(drinkAPI);
+  }
+
+  function getWeather(requestURL) {
+    fetch(requestURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // set the state for weather
+        console.log(
+          "Here is the Weather data returned from the fetch call: ",
+          data
+        );
+        updateWeather(data);
+      });
+
+    if (city) {
+    }
+  }
+
+  
+  
+  
+
+  function getDrink() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(
+          "Here is the Drink data returned from the fetch call: ",
+          data
+        );
+        // console.log(drink.drinks.idDrink);
+        updateDrink(data);
+        // updateDrink(data);
+      });
   }
 
   // function getRandomInt(max) {
@@ -105,7 +144,7 @@ const SearchDrinks = () => {
         </div>
         <button
           id="weatherButton"
-          className="button is-primary is-outlined has-text-black is-rounded city-button"
+          className="button is-primarywhite is-outlined has-text-black is-rounded city-button"
           onClick={() => {
             getTheWeather();
             getTheDrink();
@@ -115,257 +154,103 @@ const SearchDrinks = () => {
           Pick Your Poison
         </button>
       </div>
-      <div class="container" id="issueContainer">
-        <p class="is-size-4 has-text-centered has-text-primary has-background-primary-light">
-          <br />
+      <div className="container" id="issueContainer">
+        <p className="is-size-4 has-text-centered has-text-primary has-background-primary-light p-2">
           {weather ? "Here is the weather for: " + weather.name : ""}
+          
         </p>
+        
       </div>
-      <div>
+      <div className=" has-text-centered has-text-primary p-2">
         <p>
-          Weather Condition: {weather.weather ? weather.weather[0].main : ""} -{" "}
-          {weather.weather ? weather.weather[0].description : ""}
+          Weather Condition: <span className="textCopy"> {weather.weather ? weather.weather[0].main : ""} - {weather.weather ? weather.weather[0].description : ""} </span>
           <br />
-          Current Temperature: {weather.main ? weather.main.temp : ""}{" "}
+          Current Temperature: <span className="textCopy">{weather.main ? weather.main.temp : ""}</span>
         </p>
       </div>
-
-      <div class="columns is-centered is-desktop">
-        <div class="column is-6">
-          <div class="tile is-parent has-text-centered p-5">
-            <article class="tile is-child box">
-              <p
-                id="drink1Name"
-                class="drinkNames is-size-2 mb-5 has-text-primary"
-              >
-                {drink.drinks ? "Drink Title: " + drink.drinks[0].strDrink : ""}
-              </p>
-              <figure class="image is-4by3">
-                {drink.drinks
-                  ? "Drink Title: " + drink.drinks[0].strDrinkThumb
-                  : ""}
-                <img
-                  id="image"
-                  src={drink.drinks ? drink.drinks[0].strDrinkThumb : ""}
-                  alt="Placeholder"
-                />
-              </figure>
-              <button
+      <div className="columns is-centered is-desktop">
+        <div className="column">
+          <div className="tile is-parent has-text-left p-5">
+            <article class=" is-child box">
+             
+              <div className="columns is-mobile" >
+                {/* Drink Image */}
+                <div className="column">
+                  <img
+                    id="image" className="image"
+                    src={drink.drinks ? drink.drinks[14].strDrinkThumb : ""}
+                    alt={drink.drinks ? drink.drinks[14].strDrink : ""}
+                  />
+                </div>
+                {/* Drink Title */}
+                <div class="column">
+                  <div  className=" is-size-2 has-text-primary ">
+                      {drink.drinks ? " Drink Title: " + drink.drinks[14].strDrink : ""}
+                  </div>
+                  {/* Ingredients */}
+                  <div className="drinkDiv">
+                    Ingredients:
+                    <ul>
+                      <li>{drink.drinks != null ?  drink.drinks[14].strIngredient1 + " " + drink.drinks[14].strMeasure1 : " "}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient2 + " " + drink.drinks[14].strMeasure2 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient3 + " " + drink.drinks[14].strMeasure3 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient4 + " " + drink.drinks[14].strMeasure4 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient5 + " " + drink.drinks[14].strMeasure5 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient5 + " " + drink.drinks[14].strMeasure5 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient6 + " " + drink.drinks[14].strMeasure6 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient7 + " " + drink.drinks[14].strMeasure7 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient8 + " " + drink.drinks[14].strMeasure8 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient9 + " " + drink.drinks[14].strMeasure9 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient10 + " " + drink.drinks[14].strMeasure10 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient11 + " " + drink.drinks[14].strMeasure11 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient12 + " " + drink.drinks[14].strMeasure12 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient13 + " " + drink.drinks[14].strMeasure13 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient14 + " " + drink.drinks[14].strMeasure14 : ""}</li>
+                      <li>{drink.drinks != null ? drink.drinks[14].strIngredient15 + " " + drink.drinks[14].strMeasure15 : ""}</li>
+                    </ul>
+                  </div>
+                  {/* Drink Instructions */}
+                  <div className="drinkDiv">
+                    {drink.drinks != null ? "Instructions: "  + drink.drinks[14].strInstructions : ""}
+                  </div>
+                  {/* Drink Glass Type Suggestion */}
+                  <div className="drinkDiv">
+                    {drink.drinks != null ? "Glass Type Suggestion: "  + drink.drinks[14].strGlass : ""}
+                  </div>
+                  {/* Category */}
+                  <div className="drinkDiv">
+                    {drink.drinks != null ? "Category: "  + drink.drinks[14].strCategory : ""}
+                  </div>
+                  {/* Non or Alcoholic */}
+                  <div className="drinkDiv">
+                    {drink.drinks != null ? "Non or Alcoholic : "  + drink.drinks[14].strAlcoholic : ""}
+                  </div>
+                </div>
+            </div>
+              
+            
+              {/* <button
                 id="btn"
-                className="button is-primary is-outlined has-text-black is-rounded mt-4 recipe-button"
+                className="button is-primarywhite is-outlined has-text-black is-rounded city-button mt-4 recipe-button"
                 // onClick={selectDrink}
               >
+                
                 <Link
                   to={{
                     pathname: "/SingleDrink",
-                    //state: updateDrink(data),
+                    state: { drinks: drink[0] },
                   }}
                 >
                   {" "}
                   View Recipe{" "}
                 </Link>
-                View Recipe
-              </button>
+              </button> */}
             </article>
           </div>
         </div>
       </div>
     </div>
   );
-
-  // useEffect(() => {
-  //   axios.get(weatherURL).then(
-  //     (event) => {
-  //       getUserInputE1(event.target.value).then((response) => {
-  //         getCity(response.data);
-  //         getClouds(response.data);
-  //         getTemp(response.data);
-  //         getWind(response.data);
-  //         // getMyWeather(response.data);
-  //         getWeatherButton(response.data);
-  //         getUserInputE1(response.target.value);
-  //       });
-  //     },
-  //     [weatherURL]
-  //   );
-
-  //   useEffect(() => {
-  //     axios.get(cocktailURL).then((response) => {
-  //       getDrink(response.data);
-  //       getDrink1NameEl(response.data);
-  //       getdrink1NameModal(response.data);
-  //       getIngredientsEl(response.data);
-  //       getRecipeEl(response.data);
-  //     });
-  //   }, [cocktailURL]);
-  // });
-
-  // function matchIngredient(code) {
-  //   // var code = prompt("Please enter your city", "Ex: Chapel Hill");
-  //   console.log(code + " is in matchingredient function");
-  //   const ingredientArr = [
-  //     {
-  //       min: 200,
-  //       max: 299,
-  //       spirit: "bourbon",
-  //     },
-  //     {
-  //       min: 300,
-  //       max: 399,
-  //       spirit: "whisky",
-  //     },
-  //     {
-  //       min: 500,
-  //       max: 599,
-  //       spirit: "gin",
-  //     },
-  //     {
-  //       min: 600,
-  //       max: 699,
-  //       spirit: "vodka",
-  //     },
-  //     {
-  //       min: 700,
-  //       max: 799,
-  //       spirit: "brandy",
-  //     },
-  //     {
-  //       min: 800,
-  //       max: 802,
-  //       spirit: "rum",
-  //     },
-  //     {
-  //       min: 802,
-  //       max: 805,
-  //       spirit: "tequila",
-  //     },
-  //   ];
-  //   ingredientArr.forEach((obj) => {
-  //     if (code >= obj.min && code < obj.max) {
-  //       var drink = obj.spirit;
-  //       console.log(drink);
-
-  //       getDrink(drink);
-  //       // return obj.spirit
-  //     }
-  //   });
-  //   if (ingredientArr.length > 0) {
-  //     return ingredientArr[0];
-  //   }
-
-  //   return false;
-  // }
-
-  // function getDrink(drink) {
-  //   console.log(drink + "is in get drink function");
-  //   var drinkAPI =
-  //     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink;
-  //   //https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
-  //   //"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drink;
-  //   console.log(drinkAPI);
-  //   fetch(drinkAPI)
-  //     .then(function (response) {
-  //       return response.json();
-  //     })
-  //     .then(function (data) {
-  //       console.log(data);
-  //       //   console.log(data.drinks[0].idDrink);
-  //       //   console.log(data.drinks[0].strDrinkThumb);
-
-  //       function getRandomInt(max) {
-  //         return Math.floor(Math.random() * Math.floor(max));
-  //       }
-  //       // console.log(getRandomInt(data.drinks.length));
-  //       var randomInt = getRandomInt(data.drinks.length);
-  //       console.log(randomInt);
-  //       /////////////////////////////
-
-  //       //************question for Sean here
-  //       var drink1Name = data.drinks[randomInt].strDrink;
-  //       //************question for Sean here^^^^^^^^^^^
-
-  //       console.log(drink1Name);
-  //       var drink1API =
-  //         "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" +
-  //         drink1Name;
-  //       drink1NameEl.textContent = drink1Name;
-  //       drink1NameModal.textContent = drink1Name;
-  //       fetch(drink1API)
-  //         .then(function (response) {
-  //           return response.json();
-  //         })
-  //         .then(function (data) {
-  //           console.log(data);
-
-  //           var recipeArray = [
-  //             {
-  //               recipe: data.drinks[0].strInstructions,
-  //               picture: data.drinks[0].strDrinkThumb,
-  //             },
-  //           ];
-
-  //           var ingredientArray = [
-  //             data.drinks[0].strIngredient1,
-  //             data.drinks[0].strIngredient2,
-  //             data.drinks[0].strIngredient3,
-  //             data.drinks[0].strIngredient4,
-  //             data.drinks[0].strIngredient5,
-  //             data.drinks[0].strIngredient6,
-  //             data.drinks[0].strIngredient7,
-  //           ];
-  //           recipeEl.textContent = recipeArray[0].recipe;
-  //           console.log(recipeArray);
-  //           ingredientsEl.textContent = ingredientArray;
-  //           console.log(ingredientArray);
-  //           console.log(ingredientArray);
-  //           document.getElementById("image").src = data.drinks[0].strDrinkThumb;
-
-  //           // var recipe1 = data.drinks[0].strInstructions;
-  //           // var glass1 = data.drinks[0].strGlass;
-  //           // var alcohol1 = data.drinks[0].strInstructions;
-  //           // var recipe1 = data.drinks[0].strInstructions
-  //           // console.log(recipe1);
-  //         });
-  //     });
-  // }
-
-  function getWeather(requestURL) {
-    fetch(requestURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        // set the state for weather
-        console.log(
-          "Here is the Weather data returned from the fetch call: ",
-          data
-        );
-        updateWeather(data);
-      });
-
-    if (city) {
-    }
-  }
-
-  function randomDrink() {
-    const getRandomInt = Math.floor(Math.random() * Math.floor);
-    return getRandomInt(drink.drinks.length);
-  }
-
-  function getDrink() {
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(
-          "Here is the Drink data returned from the fetch call: ",
-          data
-        );
-        // console.log(drink.drinks.idDrink);
-        updateDrink(data);
-      });
-  }
 };
 
 export default SearchDrinks;
